670701e4871232be7d180d5457489974bc248698
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2024-12-11 23:35:50 +0400

    test axios

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -15,7 +15,7 @@ export const LoginPage = () => {
   const test = async () => {
     try {
       const response = await axios.get(
-        "https://game-trace-be.onrender.com/",
+        "https://game-trace-be.onrender.com/", null, 
         {
           withCredentials: true, // Include cookies for session management
         }
